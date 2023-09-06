63942228c776d92a793e3e917d684e6ffe1fab9a
egarcia28 <egarcia28@charlottesvilleschools.org>
2023-09-06 10:28:03 -0400

    onboard led blink

--- raspberry-pi/LED_blink.py
+++ raspberry-pi/LED_blink.py
@@ -3,5 +3,11 @@ import board
 import digitalio
 import.time
 
-led = digitalio.DigitalInOut(board.GP2)
+led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
+
+    while True:
+        led.value = True
+        time.sleep(1)
+        led.value = False
+        time.sleep(1)
--- raspberry-pi/code.py
+++ raspberry-pi/code.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+import board
+import digitalio
+import time
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    led.value = True
+    time.sleep(1)
+    led.value = False
+    time.sleep(1)
+    print("dskjhlksdjh")
