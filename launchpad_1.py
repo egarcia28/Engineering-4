256418d11dde47fce09542acfc2cc27d9f36779d
egarcia28 <egarcia28@charlottesvilleschools.org>
2023-09-07 10:26:28 -0400

    launchpad 2

---
+++ launchpad_1.py
@@ -0,0 +1,8 @@
+import board
+import digitalio
+import time
+
+for x in range (10): #prints numbers 1-10 in ascending order
+    print(10-x)      #changes to print numbers 1-10 in descending order
+    time.sleep(1)    # 1 second delay between numbers
+print ("liftoff")
