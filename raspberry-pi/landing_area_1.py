1e1d2f1478b41772b842731e2376cc13d0b22b3d
egarcia28 <egarcia28@charlottesvilleschools.org>
2023-11-01 09:23:36 -0400

    sdfghjk

---
+++ raspberry-pi/landing_area_1.py
@@ -0,0 +1,36 @@
+import time
+import board
+
+def total_area (set1, set2, set3):
+    try:
+        set1 = set1.split(',')
+        set2 = set2.split(',')
+        set3 = set3.split(',')
+
+        x1 = float(set1[0])
+        y1 = float(set1[1])
+        x2 = float(set1[0])
+        y2 = float(set1[1])
+        x3 = float(set1[0])
+        y3 = float(set1[1])
+
+        area = abs((x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))/2)
+
+        return area
+
+    except:
+        print("These points are invalid, try again")
+        area = 0
+        return area
+
+while True:
+    in1 = input("Enter the first (x,y) coordinate")
+    in2 = input("Enter the second (x,y) coordinate")
+    in3 = input("Enter the third (x,y) coordinate")
+
+    area = total_area(in1, in2, in3)
+
+    if area == 0:
+        continue
+    else:
+        print(f"The area of the triangle with verticies ({in1}), ({in2}), ({in3}) is {area} square km.")
