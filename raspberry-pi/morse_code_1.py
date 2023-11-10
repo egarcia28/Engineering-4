522a674de040c39b1104105178d610a265789791
egarcia28 <egarcia28@charlottesvilleschools.org>
2023-11-10 10:25:39 -0500

    sadfasd

--- raspberry-pi/morse_code_1.py
+++ raspberry-pi/morse_code_1.py
@@ -11,16 +11,17 @@ MORSE_CODE = { 'A':'.-', 'B':'-...',
     '4':'....-', '5':'.....', '6':'-....',
     '7':'--...', '8':'---..', '9':'----.',
     '0':'-----', ', ':'--..--', '.':'.-.-.-',
-    '?':'..--..', '/':'-..-.', '-':'-....-',
-    '(':'-.--.', ')':'-.--.-', ' ': ':'}
+    '?':'..--..', '/':'-..-.', '-':'-....-',            #collection of letters and their morse code equivalents 
+    '(':'-.--.', ')':'-.--.-', ' ': '/'}                #added ' ':'/' to add a slash in -between words
 
-string = (input())
-string = string.upper()
+
 while True:
-    string = input("Enter message or -Q to quit")
-    if string == '-Q':
+    string = input("Enter message or -Q to quit: ")
+    if string == '-Q':                                  #breaks while true if -Q is input
         break
-string = string.upper()
-for x in range(len(string)):
-        print(f"{MORSE_CODE[string[x]]} ")
-        
+
+    string = string.upper()
+    temp = ""
+    for x in range(len(string)):                        #for each letter, adds morse code equivalent to string
+        temp += MORSE_CODE[string[x]] + " "
+    print(f"{temp} ")                                   #f string allows to add space in between each letter
